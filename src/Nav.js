faddf5563b96e9e407942a9daf530b47c40af141
ahmedalthagafi <a.althagafi92@gmail.com>
2019-07-07 13:00:58 +0300

    accept both changes

---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+
+
+
+
+const Header = () =>{
+    return(
+        <header>
+            <h1>Hello</h1><br/>
+            <hr/>
+        </header>
+    )
+}
+
+
+export default Header
