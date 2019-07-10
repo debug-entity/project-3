e200dd2088b3d4a6a84383dbb1a2d2b4b844b385
saeedalzahrani <saeedalzahrani103@gmail.com>
2019-07-10 17:22:10 +0300

    Updates

---
+++ precache-manifest.ab46960c166f6782adef98b940df9e35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3eb828b480f16dac1e7dd47ea55807",
+    "url": "/project-3/index.html"
+  },
+  {
+    "revision": "d1bb21e0825914684461",
+    "url": "/project-3/static/css/main.987f3aa4.chunk.css"
+  },
+  {
+    "revision": "bea488cc3cc57058d0d5",
+    "url": "/project-3/static/js/2.9ea713ac.chunk.js"
+  },
+  {
+    "revision": "d1bb21e0825914684461",
+    "url": "/project-3/static/js/main.702c1026.chunk.js"
+  },
+  {
+    "revision": "7763db6ab8e7687ed2e1",
+    "url": "/project-3/static/js/runtime~main.6a465fa7.js"
+  }
+]);
